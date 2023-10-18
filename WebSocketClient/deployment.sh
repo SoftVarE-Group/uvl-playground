3766362789a08a5a00f69e8a5bfc6242df592f13
Stefan <svill33@gmail.com>
2023-10-18 18:34:18 +0200

    changed feault env value

--- WebSocketClient/deployment.sh
+++ WebSocketClient/deployment.sh
@@ -4,4 +4,4 @@ docker kill uvl-playground-website
 docker rm uvl-playground-website
 
 docker build -t uvl-playground-website .
-docker run --name uvl-playground-website -d -p 80:80 -e PORT=30000 -e UVLS_HOST_NAME=http://590c9306-8ced-48f6-85f2-bb8caa1bfd52.ul.bw-cloud-instance.org/ -t uvl-playground-website
+docker run --name uvl-playground-website -d -p 80:80 -t uvl-playground-website
