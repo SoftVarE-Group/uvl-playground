4fba09cb8d86ee7b2c5a81620c8ff35323812d0f
Stefan <svill33@gmail.com>
2023-10-20 10:32:29 +0200

    changed port of dockercontainer to introduce a https reverse proxy

--- WebSocketClient/deployment.sh
+++ WebSocketClient/deployment.sh
@@ -4,4 +4,4 @@ docker kill uvl-playground-website
 docker rm uvl-playground-website
 
 docker build -t uvl-playground-website .
-docker run --name uvl-playground-website -d -p 80:80 -t uvl-playground-website
+docker run --name uvl-playground-website -d -p 8000:80 -t uvl-playground-website
