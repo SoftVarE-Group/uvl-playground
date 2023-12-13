03e3af8426c4ee0189a92b4f151caa73b5df2a0b
Stefan <svill33@gmail.com>
2023-12-13 07:55:00 +0100

    feat(client): new fancy background for intro

--- WebSocketClient/src/intro.ts
+++ WebSocketClient/src/intro.ts
@@ -33,7 +33,17 @@ export const initIntroJS = () => {
         intro.setOption("exitOnOverlayClick", false);
         intro.setOption("overlayOpacity", 0);
         intro.setOption("disableInteraction", true);
+        intro.setOption("highlightClass", "tmpClass");
         intro.start();
+
+        var helperLayer = document.getElementsByClassName("tmpClass");
+        setTimeout(() => {
+            if(helperLayer[0] instanceof HTMLElement){
+                helperLayer[0].style["box-shadow"] = "rgb(255, 255, 255) 0px 0px 1px 2px, rgba(230, 230, 230, 0.44) 0px 0px 0px 5000px";
+            }
+        }, 100);
+            
+        
     });
 
 }
