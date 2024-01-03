049a1325e265959d0720d50511f7830600dc50a6
JannisDommer <jannis.dommer@uni-ulm.de>
2024-01-03 17:56:02 +0100

    feat(examples): added useful examples to the dropdown

--- WebSocketClient/src/examples.ts
+++ WebSocketClient/src/examples.ts
@@ -7,10 +7,73 @@ let globalEditor: editor.IStandaloneCodeEditor|null;
 export function initExamples(editor: editor.IStandaloneCodeEditor) {
     globalEditor = editor;
     loadDefaultExample();
+    loadIceCreamExample();
+    loadComputerExample();
 }
 
-const defaultExampleText = "features\n\tfeature1\n\t\tor\n\t\t\tfeature2\n\t\t\tfeature3\n\nconstraints\n\tfeature1";
+const defaultExampleText = `features
+    feature1
+        or
+            feature2
+            feature3
+    
+constraints
+feature1`;
+const iceCreamExampleText = `features
+    "Ice Cream" {extended__}
+        mandatory
+            Category
+                alternative
+                    Popsicle {Price 1}
+                    Scoop {Price 2}
+            Flavors
+                or
+                    Lemon
+                    Chocolate cardinality [1..3]
+                        alternative
+                            White
+                            Dark
+            Container
+                alternative
+                    Stick
+                    Cup
+                    Cone
 
+        optional
+            Waffle {Price 0.7}
+            String "Name of customer"
+
+constraints
+    Popsicle => Stick
+    Scoop => Cup | Cone
+    sum(Price) < 2.5`;
+
+const computerExampleText = `features
+    Computer {abstract}
+        mandatory
+            CPU {Power 100, Manufacturer 'Intel'}
+            "Graphics Card"
+                or
+                    Dedicated {powerConsumption 300}
+                    Integrated {powerConsumption 100}
+            Cooling
+                alternative
+                    Liquid
+                    Air
+            PSU
+                alternative
+                    StrongPSU
+                    WeakPSU
+        optional
+            "SATA-Devices"
+                [0..2]
+                    HDD {powerConsumption 10}
+                    SSD {powerConsumption 5}
+                    "DVD-Drive" {powerConsumption 5}
+
+constraints
+    Dedicated => Liquid
+    sum(powerConsumption) > 300 => StrongPSU`
 
 function loadDefaultExample() {
     const button: HTMLButtonElement|null = document.querySelector("#defaultExample");
@@ -20,6 +83,34 @@ function loadDefaultExample() {
             if (opsModel) {
                 const fullModelRange = opsModel.getFullModelRange();
                 const operation: IIdentifiedSingleEditOperation = {text: defaultExampleText, range: fullModelRange};
+                opsModel.applyEdits([operation], false);
+            }
+        }
+    }
+}
+
+function loadIceCreamExample() {
+    const button: HTMLButtonElement|null = document.querySelector("#iceCreamExample");
+    if(button){
+        button.onclick = () => {
+            const opsModel = globalEditor?.getModel();
+            if (opsModel) {
+                const fullModelRange = opsModel.getFullModelRange();
+                const operation: IIdentifiedSingleEditOperation = {text: iceCreamExampleText, range: fullModelRange};
+                opsModel.applyEdits([operation], false);
+            }
+        }
+    }
+}
+
+function loadComputerExample() {
+    const button: HTMLButtonElement|null = document.querySelector("#computerExample");
+    if(button){
+        button.onclick = () => {
+            const opsModel = globalEditor?.getModel();
+            if (opsModel) {
+                const fullModelRange = opsModel.getFullModelRange();
+                const operation: IIdentifiedSingleEditOperation = {text: computerExampleText, range: fullModelRange};
                 opsModel.applyEdits([operation], false);
             }
         }
