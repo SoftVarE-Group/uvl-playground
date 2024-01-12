b00201dc6733fbc324a6381b333020df43ea2fba
Stefan <svill33@gmail.com>
2024-01-12 08:37:49 +0100

    fix: playground tour

--- WebSocketClient/src/intro.ts
+++ WebSocketClient/src/intro.ts
@@ -14,11 +14,11 @@ export const initIntroJS = () => {
             element: "[id='1']",
             intro: 'For example, click here to visualize your feature model. The feature model on the right is then automatically updated when you edit the model. Click on it again to hide it.'
         }, {
-            element: "#separator", intro: 'You can change the size of the editor and the visualization with your mouse.'
+            element: "#separator1", intro: 'You can change the size of the editor and the visualization with your mouse.'
         }, {
             element: "#uvl-tutorialButton", intro: 'If you are not familiar with UVL or need a little refresh, click here to get a quick tutorial of the language.'
         }, {
-            element: "#example-dropdown", intro: 'Click here to load an UVL example from a list of different examples.'
+            element: ".dropdown-wrapper", intro: 'Click here to load an UVL example from a list of different examples.'
         }],
     });
 
